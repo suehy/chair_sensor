a040e39bd387f39341c91580416d553f35f2da7e
Sue <choy.sueyin@gmail.com>
2019-03-26 16:26:23 +0100

    preprocess data with sliding window option

--- ml_project/src/data/prepare_data.py
+++ ml_project/src/data/prepare_data.py
@@ -5,6 +5,7 @@ import datetime
 from os import listdir
 from pandas import read_json
 from pandas import DataFrame
+import pandas as pd
 import re
 import csv
 
@@ -13,8 +14,9 @@ import csv
 @click.argument('dest')
 @click.argument('freq')
 @click.option('--win')
-def main(source, dest, freq, win):
-    """ Preprocess data for model training """
+@click.option('--test')
+def main(source, dest, freq, win, test):
+    """ Preprocess data for model training and split into train/test set """
 
     logger = logging.getLogger(__name__)
     logger.info('Preprocessing data from ' + source)
@@ -25,22 +27,43 @@ def main(source, dest, freq, win):
     for name in subjects:
         subjects[name].sort(key=lambda x: datetime.datetime.strptime(x['timestamp'], format))
 
-    #shift_data(subjects, win)
+    if win == None:
+        win = '1'
 
-    # Saving separate csv for each subject
-    save_as_csv(subjects, dest, freq, win)
+    shifted_data = {}
+    for name in subjects:
+        data = pd.DataFrame.from_dict(subjects[name])
+        shifted_data[name] = shift_data(data, int(win))
+        # Saving separate csv for each subject
+        save_as_csv(shifted_data[name], name, dest, freq, win)
 
-#def shift_data():
+    # if not test == None:
+    #     i = 0
 
+def split_data(test_size):
+    return 0
 
-def save_as_csv(data, dest, freq, win):
-    for subj in data:
-        with open(dest + '/' + subj + freq + 'hz.csv', 'w') as outfile:
-            f = csv.writer(outfile)
-            f.writerow(['x', 'y', 'z', 'state'])
-            for d in data[subj]:
-                f.writerow([d['x'], d['y'], d['z'], d['state']])
-            outfile.close()
+def shift_data(df, win):
+    shifted_df = DataFrame()
+    for i in range(0, win):
+        shifted_df['x' + str(i)] = df['x'].shift(i)
+        shifted_df['y' + str(i)] = df['y'].shift(i)
+        shifted_df['z' + str(i)] = df['z'].shift(i)
+    shifted_df['state'] = df['state']
+    shifted_df.drop(df.index[:win-1], inplace=True)
+    return shifted_df
+
+def save_as_csv(df, subj, dest, freq, win):
+    filename = dest + '/' + subj + win + '_' + freq + 'hz.csv'
+    df.to_csv(filename, index=False)
+    #for subj in data:
+        # with open(dest + '/' + subj + freq + win + 'hz.csv', 'w') as outfile:
+        #     f = csv.writer(outfile)
+        #     # f.writerow(['x', 'y', 'z', 'state'])
+        #     # for d in data:
+        #     #     print(data)
+        #     #     f.writerow([d['x'], d['y'], d['z'], d['state']])
+        #     outfile.close()
 
 def load_dataset(source, freq):
     subjects = {}
