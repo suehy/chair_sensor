501c8dff7de5330ca3eff24d72f7cc4f2804dc56
Sue <choy.sueyin@gmail.com>
2019-06-12 14:48:02 +0200

    updated mqtt subscriber module

--- clients/mqttClient/subscriber.js
+++ clients/mqttClient/subscriber.js
@@ -1,5 +1,48 @@
 var mqtt = require('mqtt');
-var client = mqtt.connect('mqtt:localhost:1883', {clientId: 'subscriber'});
+
+var self, client;
+
+Subscriber = function Subscriber(options) {
+    this.options = options;
+};
+
+Subscriber.prototype.connectToBroker = function(broker, clientId, options, successCb) {
+    console.log(clientId + ' connecting to MQTT ' + broker);
+
+    var client = mqtt.connect(broker, {clientId: clientId});
+
+    return new Promise((resolve, reject) => {
+
+        client.on('connect', () => {
+            this.client = client;
+            resolve(successCb());
+        });
+
+        client.on('error', (error) => {
+            reject(error);
+        });
+    });
+}
+
+Subscriber.prototype.subscribe = function(topic, options, cb) {
+    return new Promise((resolve, reject) => {
+        self.client.subscribe(topic, (err) => {
+            if (!err) {
+                self.client.on('message', (topic, msg) => {
+                    console.log(msg.toString());
+                });
+                resolve('success');
+            }
+            else {
+                reject(err);
+            }
+        });
+    });
+}
+
+Subscriber.prototype.end = function() {
+    self.client.end();
+}
 
 client.on('connect', function () {
     client.subscribe('presence', function (err) {
@@ -12,3 +55,8 @@ client.on('connect', function () {
         }
     })
 })
+
+module.exports = function(options) {
+    self = new Subscriber(options);
+    return self;
+}
